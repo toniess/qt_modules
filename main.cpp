d7bf341a71ca736d6bb681c33ad032f5d22db3ea
Ignatiy Glushkov <ignat.glushkov@mail.ru>
2024-03-25 00:38:42 +0300

    changes

--- main.cpp
+++ main.cpp
@@ -1,18 +1,13 @@
-#include "mainwindow.h"
-
-#include <QApplication>
+#include <QCoreApplication>
 #include <QNetworkAccessManager>
 #include <QSqlDatabase>
 
-
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QCoreApplication a(argc, argv);
 
     QNetworkAccessManager networkNamager;
     QSqlDatabase database;
 
-    MainWindow w(nullptr, &networkNamager, &database);
-    w.show();
     return a.exec();
 }
